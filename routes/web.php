499b50a8cab99df918aee2216deeff4a23743494
Lluis Casamajor <lluischess@gmail.com>
2022-05-10 21:46:41 +0200

    Add phpinfosoft

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Show PHP:
+Route::get('/php', function(){
+    return view('phpinfo');
+});
