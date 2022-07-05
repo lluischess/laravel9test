46615fae9b3fc13eb4ca074518dc1ce542f7919f
Lluis Casamajor <lluischess@gmail.com>
2022-07-05 22:18:11 +0200

    Add nav for session users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,12 +55,25 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a href="" class="nav-link">Home</a>
+                            </li>
+                            <li class="nav-item">
+                            <a href="" class="nav-link">Add image</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="">
+                                        {{ __('Profile') }}
+                                    </a>
+                                    <a class="dropdown-item" href="">
+                                        {{ __('Setting') }}
+                                    </a>   
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -70,6 +83,8 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
                                     </form>
+
+                                    
                                 </div>
                             </li>
                         @endguest
