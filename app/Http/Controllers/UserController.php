d9fa8f5de6e97a64aeab945b8036901c39214638
Lluis Casamajor <lluischess@gmail.com>
2022-07-10 21:33:12 +0200

    Create controller user settings validate all filds

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Validator;
 
 use Illuminate\Http\Request;
 
@@ -11,8 +12,20 @@ class UserController extends Controller
     }
 
     public function update(Request $request){
-       # Como tenemos el namespace en controllers y la carpeta Auth esta dentro le añadimos una contrabarra \Auth
-       $id = \Auth::user()->id;
+
+        # Como tenemos el namespace en controllers y la carpeta Auth esta dentro le añadimos una contrabarra \Auth
+        $id = \Auth::user()->id;
+        
+        // validate for backend data
+        $validate = $this->validate($request, [
+            'name' => ['required', 'string', 'max:255'],
+            'surname' => ['required', 'string', 'max:255'],
+            'nick' => ['required', 'string', 'max:255', 'unique:users,nick,'.$id], // es una excepcion que el nick coincida con el $id
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,'.$id]
+        
+        ]);
+       
+       
        $name = $request->input('name');
        $surname = $request->input('surname');
        $nick = $request->input('nick');
