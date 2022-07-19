b33f16d337a43fda8a11a315856e375c0914f8a2
Lluis Casamajor <lluischess@gmail.com>
2022-07-19 11:58:16 +0200

    Creating new functionality to comment a publication

--- resources/views/images/detail.blade.php
+++ resources/views/images/detail.blade.php
@@ -11,7 +11,7 @@
             </div>
             @endif
 
-            
+
             <div class="card pub_img">
                 <div class="card-header">
                     @if(isset($image->user->img))
@@ -31,21 +31,35 @@
                         <img src="{{ route('image.file', ['filename' => $image->image_path]) }}">
                     </div>
                     <div class="likes">
-                        
+
                         <img src="{{ asset('images/me-gustaNegro.png') }}">
                     </div>
                     <div class="container-desc">
                         <span>{{ '@'.$image->user->nick }}</span>
                         <p>{{ $image->description }}</p>
                     </div>
-
-                    <a href="" class="btn btn-warning">
-                        Coments ({{ count($image->comments) }})
-                    </a>
-
                     <div class="date">
                         <p>Published: <span>{{ FormatTime::LongTimeFilter($image->created_at)}} </span></p>
                     </div>
+                    
+                    <h2>
+                        Coments ({{ count($image->comments) }})
+                    </h2>
+                    <hr>
+
+                    <div>
+                        <form action="POST" action="">
+                            <input type="hidden" name="image_id" value="{{ $image->id}}">
+
+                            <p> Coment:
+                                <textarea name="coment" id="coment" class="form-control"></textarea>
+                            </p>
+                            <input type="button" class="btn btn-info" value="Post coment">
+                        </form>
+
+                    </div>
+
+
                 </div>
             </div>
         </div>
